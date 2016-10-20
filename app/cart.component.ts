dbc25a494b7798b142392c59ff12713f107cdba3
April Peng and Katrina Hirsch <april+katrina@>
2016-10-20 11:57:26 -0700

    add checkout button

--- app/cart.component.ts
+++ app/cart.component.ts
@@ -4,14 +4,21 @@ import { Album } from './album.model';
 @Component({
   selector: 'cart-display',
   template: `
-    <ul *ngFor="let item of purchases">
-      <li>
-      <img (click)="removeAlbum(item)" id="remove" src="./../resources/images/remove.png">
-      {{ item.name }} {{ "($" + item.price + ".00)" }}
-      </li>
-    </ul>
+    <div class="row" *ngFor="let item of purchases">
+      <div class="col-sm-1">
+        <img (click)="removeAlbum(item)" id="remove" src="./../resources/images/remove.png">
+      </div>
+      <div class="col-sm-6">
+      {{ item.name }}
+      </div>
+      <div id="price" class="col-sm-4">
+      {{ "$" + item.price + ".00" }}
+      </div>
+    </div>
     <hr>
-    <h4><strong>Total: {{ "$" + total + ".00" }}</strong></h4>
+    <h4><strong>Total: {{ "$" + total + ".00" }}
+    <button class="btn pull-right" id="checkout">Checkout</button>
+    </strong></h4>
   `
 })
 
